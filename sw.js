08d2a37693eb568b39b0017c5eb69c58794dce19
Kamikadzus <Kamikadzus96@gmail.com>
2025-07-03 22:10:14 +0300

    Add service worker

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'bsu-premium-v1';
+const urlsToCache = [
+  '/Bsu-premium-calculator/',
+  '/Bsu-premium-calculator/index.html',
+  '/Bsu-premium-calculator/manifest.json',
+  '/Bsu-premium-calculator/icons/icon-192.png',
+  '/Bsu-premium-calculator/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
